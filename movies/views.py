560e57f9f019121603da750f6ea85a318b760506
shashank7t <shashank7ishant@gmail.com>
2018-06-02 18:06:14 -0500

    Good looking UI with series of movies pulled from DB

--- movies/views.py
+++ movies/views.py
@@ -3,4 +3,6 @@ from movies.models import Movies
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'movies/home_page.html')
+    result_set = Movies.objects.all()
+    context = {'result_set': result_set}
+    return render(request, 'movies/home_page.html', context)
