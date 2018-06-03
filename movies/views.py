2707c351e18c130ec8efde67eccdb4174ecefbee
shashank7t <shashank7ishant@gmail.com>
2018-06-02 20:29:13 -0500

    Added search functionality that pulls up movies according to the user query.

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from movies.models import Movies
 
+
 # Create your views here.
 def home_page(request):
-    result_set = Movies.objects.all()
+    user_query = str(request.GET.get('query', ''))
+    result_set = Movies.objects.filter(Name__icontains=user_query)
     context = {'result_set': result_set}
     return render(request, 'movies/home_page.html', context)
